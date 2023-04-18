2f6cf14028e06e2a60444c1818d606c52e060ef3
TUP-UTN-FRC-LCIII <tup.utn.frc.lciii@gmail.com>
2023-04-17 21:07:25 -0300

    Strategy Pattern

---
+++ strategy/src/main/java/ar/edu/utn/frc/tup/lciii/MusicPlayer.java
@@ -0,0 +1,22 @@
+package ar.edu.utn.frc.tup.lciii;
+
+import lombok.Data;
+
+@Data
+public class MusicPlayer {
+
+    private EqualizerStrategy equalizerStrategy;
+
+    public MusicPlayer(EqualizerStrategy equalizerStrategy) {
+        this.equalizerStrategy = equalizerStrategy;
+    }
+
+    public void changeEqualization(EqualizerStrategy equalizerStrategy) {
+        this.equalizerStrategy = equalizerStrategy;
+    }
+
+    public void adjustFrequency() {
+        System.out.println("Applying frequency adjustment...");
+        equalizerStrategy.adjustFrequencies();
+    }
+}
